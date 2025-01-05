8a05273027ae1f005c0a9669aeadf97d53162c44
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-05 17:00:53 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [number, setNumber] = useState("");
+
+  function handleChange(e) {
+    let num = e.target.value;
+
+    if(num.length === 1) {
+      num = "(" + num;
+    }
+
+    if(num.length === 4) {
+      num = num + ") ";
+    }
+
+    if(num.length === 9) {
+      num = num + "-";
+    }
+
+    setNumber(num);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form>
+          <input type="text" placeholder="(555) 555-5555" value={number} onChange={handleChange} />
+          <button type="submit" disabled={number.length!==14}>Submit</button>
+        </form>
     </div>
   )
 }
